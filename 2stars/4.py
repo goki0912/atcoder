5ca3eafe907b37c8f9e2f0a44c47b908f57bea1f
goki <107348301+goki0912@users.noreply.github.com>
2025-03-12 21:46:49 +0900

    fix: 2-4

--- 2stars/4.py
+++ 2stars/4.py
@@ -4,16 +4,10 @@ A = [list(map(int, input().split())) for _ in range(H)]
 row_sums = [sum(row) for row in A]
 col_sums = [sum(A[i][j] for i in range(H)) for j in range(W)]
 
-B = []
+B = [[row_sums[i] + col_sums[j] - A[i][j]  for j in range(W)] for i in range(H)]
 for i in range(H):
   for j in range(W):
     B[i][j] = row_sums[i] + col_sums[j] - A[i][j]
 
-print(B)
-
-
-# col_sums = [0] * W  # 各列の合計を格納するリストを初期化
-# for j in range(W):  # 列ごとに
-#     for i in range(H):  # 各行の値を足す
-#         col_sums[j] += A[i][j]  # 列ごとに合計を計算
-
+for i in B:
+  print(*i)
