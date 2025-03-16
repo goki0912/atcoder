2453c9d701c9989ca689c1b2ce1b56614fed65e6
goki <107348301+goki0912@users.noreply.github.com>
2025-03-16 23:51:53 +0900

    fix: 2-24

--- 2stars/24.py
+++ 2stars/24.py
@@ -2,11 +2,6 @@ N, K = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-diff_sum = 0
-for i in range(N):
-    diff_sum+= A[i] - B[i]
+diff_sum = sum(abs(A[i] - B[i]) for i in range(N))
 
-if diff_sum%2 == K%2:
-    print("Yes")
-else:
-    print("No")
+print("Yes" if K >= diff_sum and diff_sum%2 == K%2 else "No")
